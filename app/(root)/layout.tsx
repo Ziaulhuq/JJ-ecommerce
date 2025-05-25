5cdefb0cdcff9934fa71178127bf7e309a4f9c05
ZIA UL HUQ <ziaulhuq@ZIAs-MacBook-Pro.local>
2025-05-26 00:38:08 +0800

    working with headers component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Shared/Header";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,6 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <Header />
       {children}
     </div>
   );
