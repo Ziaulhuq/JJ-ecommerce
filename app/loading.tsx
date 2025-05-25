cb18163bda40aa067f654efac6398abadbc51451
ZIA UL HUQ <ziaulhuq@ZIAs-MacBook-Pro.local>
2025-05-25 23:44:12 +0800

    correction in next..config

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <p>loading pages....</p>
+    </div>
+  );
+};
+
+export default loading;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  devIndicators: false,
 };
 
 export default nextConfig;
