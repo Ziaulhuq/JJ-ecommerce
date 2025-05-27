11386e23a78817861cc38d6a0f145d2ca6680ac7
ZIA UL HUQ <ziaulhuq@ZIAs-MacBook-Pro.local>
2025-05-27 09:53:41 +0800

    initial header

--- components/Shared/Header/index.tsx
+++ components/Shared/Header/index.tsx
@@ -1,5 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import { APP_NAME } from "@/lib/constants";
+import { ShoppingCartIcon, UserIcon } from "lucide-react";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="flex justify-between border-b w-full">
+        <div className="flex justify-center items-center gap-4">
+          <Link href="/">
+            <Image
+              src="/jj-logo.png"
+              alt={`${APP_NAME} logo`}
+              width={70}
+              height={70}
+              priority={true}
+              className="flex"
+            ></Image>
+          </Link>
+          <span>JJ Handmade</span>
+        </div>
+        <div className="flex gap-4 items-center">
+          <div className="flex gap-2">
+            <ShoppingCartIcon />
+            <span>Cart</span>
+          </div>
+          <div className="flex gap-2">
+            <UserIcon />
+            <span>Admin</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
